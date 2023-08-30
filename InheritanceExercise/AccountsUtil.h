593c612431aa113e56f5e148b32039f740ef19e0
Lyudmil-Giant <86479168+Lyudmil-Giant@users.noreply.github.com>
2023-08-30 15:07:23 +0200

    bank simulator

--- InheritanceExercise/AccountsUtil.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-#include "Account.h"
-
-void Deposit(std::vector<Account> &accounts, double amountToDeposit);
-void Withdraw(std::vector<Account> &accounts, double amountToWithdraw);
-void Display(std::vector<Account> &accounts);
-
